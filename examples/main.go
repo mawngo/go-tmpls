63944080882061bce67be9f15b258d08b842f3ce
mawngo <sitdownrightnow.dev@gmail.com>
2025-10-02 00:35:33 +0700

    chore: update README.md

--- examples/main.go
+++ examples/main.go
@@ -25,15 +25,21 @@ func main() {
 		println("Dev mode enabled")
 	}
 
-	// Setup template cache and http.FileServer from root, which is embedded when dev mode is disabled.
+	// Setup template cache and http.FileServer from root,
+	// which is embedded when dev mode is disabled.
 	// You can use StandardTemplateFS to create the TemplateCache only,
-	// or NewTemplateCache(fs, options...) to create template cache if you want to use another directory for template.
+	// or NewTemplateCache(fs, options...) to create template cache
+	// if you want to use another directory for template.
 	//
-	// StandardWebFS setup TemplateCache and http.FileServer based on golang-standards/project-layout,
+	// StandardWebFS setup TemplateCache and http.FileServer
+	// based on golang-standards/project-layout,
 	// which read template from web/template and serve static files from web/static.
 	cache, static, err := tmpls.StandardWebFS(root,
-		tmpls.WithNocache(*devmode),            // Disable cache in dev mode, so we can see changes without re-run the project.
-		tmpls.WithGlobs("components/*.gohtml")) // Include all files in components by default. Those files can be referenced in using base name.
+		// Disable cache in dev mode, so we can see changes without re-run the project.
+		tmpls.WithNocache(*devmode),
+		// Include all files in components by default.
+		// Those files can be referenced in using base name.
+		tmpls.WithGlobs("components/*.gohtml"))
 	if err != nil {
 		panic(err)
 	}
@@ -41,7 +47,9 @@ func main() {
 	http.Handle("GET /static/", http.StripPrefix("/static/", static))
 	http.HandleFunc("GET /", func(res http.ResponseWriter, req *http.Request) {
 		// Paging demonstration, just empty data.
-		p := page.NewPage[any](make([]any, page.DefaultPageSize), page.DefaultPageSize*10, page.NewPaginator(req))
+		p := page.NewPage[any](make([]any, page.DefaultPageSize),
+			page.DefaultPageSize*10,
+			page.NewPaginator(req))
 
 		cache.MustExecute(res,
 			page.D{"Name": *name, "Page": p},
